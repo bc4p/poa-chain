20feccd5cd7747f4731c27fff4b3fb951318448e
Florian Maurer <maurer@fh-aachen.de>
2022-12-09 11:06:56 +0100

    update clear data

--- clear_data.sh
+++ clear_data.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-sudo rm ./data/node/* data/validator1/* data/validator2/* data/validator3/* -r
+# to remove only stuck validators
+# Unclean shutdown detected
+# weird logs like "needed geth db freezer-migrate", but freezer migrate did not help
+# geth rewinding blockchain
+sudo rm -r data/validator1/geth/chaindata
+sudo rm -r data/validator2/geth/chaindata
+sudo rm -r data/validator3/geth/chaindata
+# they are pulling data from the node, which has access to the bc history
+docker compose up -d
+
+# to delete everything and start new
+#sudo rm ./data/node/* data/validator1/* data/validator2/* data/validator3/* -r
