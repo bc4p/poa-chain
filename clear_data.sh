9e4a5f7b53061d94c19a156a2b49529751c21333
Florian Maurer <maurer@fh-aachen.de>
2022-12-28 02:40:34 +0000

    remove apps_menu

--- clear_data.sh
+++ clear_data.sh
@@ -4,9 +4,9 @@
 # Unclean shutdown detected
 # weird logs like "needed geth db freezer-migrate", but freezer migrate did not help
 # geth rewinding blockchain
-sudo rm -r data/validator1/geth/chaindata
-sudo rm -r data/validator2/geth/chaindata
-sudo rm -r data/validator3/geth/chaindata
+sudo rm -r data/validator1/geth
+sudo rm -r data/validator2/geth #/chaindata
+sudo rm -r data/validator3/geth #/chaindata
 # they are pulling data from the node, which has access to the bc history
 docker compose up -d
 
